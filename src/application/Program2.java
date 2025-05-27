e696c55d3dc767b764e3d861f39865ca48394e10
Dhomini Silva <dhominisilva1@gmail.com>
2025-05-26 23:20:15 -0300

    insert implementation department

---
+++ src/application/Program2.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("=== TESTE 1: department insert ===");
+		Department dep = new Department(null, "Apple");
+		departmentDao.insert(dep);
+		System.out.println(dep);
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
