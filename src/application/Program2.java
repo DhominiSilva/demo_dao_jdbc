58e17ef530ac6d599a6995ec05f598a41e7a8f86
Dhomini Silva <dhominisilva1@gmail.com>
2025-05-27 23:21:30 -0300

    update implementation department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -14,16 +14,24 @@ public class Program2 {
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-		System.out.println("=== TESTE 1: department insert ===");
-		//Department dep = new Department(null, "Apple");
-		//departmentDao.insert(dep);
-		//System.out.println(dep);
+//		System.out.println("=== TESTE 1: department insert ===");
+//		Department dep = new Department(null, "Apple");
+//		departmentDao.insert(dep);
+//		System.out.println(dep);
+		
+		
+		
+		System.out.println("=== TESTE 3: department update ===");
+		Department dep1 = new Department();
+		dep1 = departmentDao.findById(7);
+		dep1.setName("Watter");
+		departmentDao.update(dep1);
+		System.out.println("Update completed!");
+		
 		
 		System.out.println("=== TESTE 2: department findById ===");
-		Department department = departmentDao.findById(8);
+		Department department = departmentDao.findById(7);
 		System.out.println(department);
-		
-		
 		
 		
 		
